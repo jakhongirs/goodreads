79c25a794dfa30ecb29072f303b54d061b19ae68
jakhongirs <j.sagdullaev31@gmail.com>
2022-07-16 14:55:01 +0500

    feat: Book, Author, BookAuthor, BookReview Models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+# BOOK:
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    isbn = models.CharField(max_length=17)
+
+
+# AUTHOR:
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    bio = models.TextField()
+
+
+# BOOK_AUTHOR:
+class BookAuthor(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(Book, on_delete=models.CASCADE)
+
+
+# BOOK_REVIEW:
+class BookReview(models.Model):
+    user = models.ForeignKey(User, on_delete=Cascade)
