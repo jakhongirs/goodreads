6986dcc0554d6652e9bd501d99bb1516950ff03c
jakhongirs <j.sagdullaev31@gmail.com>
2022-07-18 20:41:16 +0500

    feat: all models

--- books/models.py
+++ books/models.py
@@ -1,4 +1,6 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # BOOK:
@@ -19,9 +21,14 @@ class Author(models.Model):
 # BOOK_AUTHOR:
 class BookAuthor(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    author = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
 
 # BOOK_REVIEW:
 class BookReview(models.Model):
-    user = models.ForeignKey(User, on_delete=Cascade)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    comment = models.TextField()
+    stars_given = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
